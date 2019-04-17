4a86ecf68ac2f5cac99489d7f407c9fd5d08998d
CEGLIK FILIP <f35332c@gescoeurope.fiatgesco.com>
2019-04-17 15:25:36 +0200

    added month based lists in English and Polish

--- pyword.py
+++ pyword.py
@@ -3,7 +3,7 @@ import datetime
 
 
 currentDate = datetime.datetime.now()
-monthsEng = (
+monthsEng = [
     'January',
     'February',
     'March',
@@ -16,40 +16,125 @@ monthsEng = (
     'October',
     'November',
     'December'
-),
+]
+monthsPl = [
+        'Styczen',
+        'Luty',
+        'Marzec',
+        'Kwiecien',
+        'Maj',
+        'Czerwiec',
+        'Lipiec',
+        'Sierpien',
+        'Wrzesien',
+        'Pazdziernik',
+        'Listopad',
+        'Grudzien'
+]
 
-#Creates a wordlist with current year ending
-def get_long_year_pass(months, endFile, passwd):
+#END of data
+
+varDict = {'monthsEng': monthsEng, 'monthsPl': monthsPl}
+
+#START of functions
+
+
+
+#Creates words with current year ending as yyyy
+def get_long_year_pass(months, endFile):
     for month in months:
-        passwd=month+currentDate.strftime("%Y")
-        endFile.Write(passwd+'\n')
+        passwd=str(month)+currentDate.strftime("%Y")
+        endFile.write(passwd+'\n')
+
+def get_long_year_pass_lower(months, endFile):
+    for month in months:
+        passwd=str(month)+currentDate.strftime("%Y")
+        endFile.write(passwd.lower()+'\n')
+
+#Creates words with current year ending as yy
+def get_short_year_pass(months, endFile):
+    for month in months:
+        passwd=month+currentDate.strftime("%y")
+        endFile.write(passwd+'\n')
+
+def get_short_year_pass_lower(months, endFile):
+        for month in months:
+                passwd=month+currentDate.strftime("%y")
+                endFile.write(passwd.lower()+'\n')
+
+
+
 
 def main():
 
 
+        wordlist = open("wordlist.txt", "w+")
+        
+        parser = ArgumentParser()
 
-    parser = ArgumentParser()
+
+        parser.add_argument("--file",
+                                action="store_true", dest="make_file", default=False,
+                                help="Creates a wordlist file. "
+                                )
+        parser.add_argument("--lang",
+                                action="store",
+                                default="Eng",
+                                dest="lang_dict",
+                                help="Limit word list to certain languages: Pl, Eng"
+                                )
+        parser.add_argument("--short",
+                                action="store_true", 
+                                dest="get_short_year",
+                                help="Add short year format"
+                                )
+        parser.add_argument("--long",
+                                action="store_true", 
+                                dest="get_long_year",
+                                help="Add long year format"
+                                )
+        parser.add_argument("--allcase",
+                                action="store_true", 
+                                dest="get_all_cases",
+                                help="Create wordlist both with regular words and lowercase words"
+                                )
+        parser.add_argument("--alllangs",
+                                action="store_true", 
+                                dest="get_all_langs",
+                                help="Create a wordlist using all supported languages"
+                                )
+
+        args = parser.parse_args()
+        words = "months" + args.lang_dict
 
 
-    parser.add_argument("--file",
-                        action="store_true", dest="make_file", default=False,
-                        help="Creates a wordlist file. "
-                       )
-    parser.add_argument("--lang",
-                        action="append",
-                        dest="lang_dict", default=None,
-                        help="Limit word list to certain languages"
-                       )
-    parser.add_argument("--year",
-                        action="store_true",
-                        dest="get_long_year_pass",
-                        help="Year to create wordlist with"
-                       )
+        print("""
+  _____    __          __           _ 
+ |  __ \   \ \        / /          | |
+ | |__) |   \ \  /\  / /__  _ __ __| |
+ |  ___/ | | \ \/  \/ / _ \| '__/ _` |
+ | |   | |_| |\  /\  / (_) | | | (_| |
+ |_|    \__, | \/  \/ \___/|_|  \__,_|
+         __/ |                        
+        |___/                         
+        
+        """)
 
-    args = parser.parse_args()
+        if args.get_short_year:
+                get_short_year_pass(varDict[words], wordlist)
 
-    if args.make_file:
-        print("todo file export")
+        if args.get_long_year:
+                get_long_year_pass(varDict[words],wordlist)
+
+        if args.get_all_cases:
+                get_short_year_pass_lower(varDict[words],wordlist)
+                get_long_year_pass_lower(varDict[words],wordlist)
+
+        #if args.get_all_langs:
+
+        
+        wordlist.close()
+        print("Wordlist created successfully!\n")
 
 if __name__ == "__main__":
     main()
